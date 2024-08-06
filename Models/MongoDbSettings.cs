34bcd2a84e6e2de4131e79e5c6830d6dbcfa654b
Pranav Chunchur <112144655+PChunchur@users.noreply.github.com>
2024-08-06 21:33:16 +0530

    Adds entryupdate UI

---
+++ Models/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+namespace MongoDb.Models;
+
+public class MongoDbSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null;
+    public string CollectionName { get; set; } = null;
+}
